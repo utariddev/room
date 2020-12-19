0ac805674220f35994e3d0a080d3831daa9b7645
utarid <utarid@protonmail.com>
2020-12-19 21:06:37 +0300

    listeleme icin recyclerview eklendi

--- app/src/main/java/org/utarid/room/MainActivity.java
+++ app/src/main/java/org/utarid/room/MainActivity.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import org.utarid.room.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private DatabaseWorker databaseWorker;
+    private List<EntityWorker> globalWorkersList;
+    private AdapterWorker mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         databaseWorker = DatabaseWorker.getInstance(this);
+
+        globalWorkersList = new ArrayList<>();
+        RecyclerView recyclerView = binding.recyclerView;
+        mAdapter = new AdapterWorker(this, globalWorkersList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
 
         binding.btnInsertWorker.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,9 +70,17 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                globalWorkersList.clear();
                 List<EntityWorker> workersList = databaseWorker.daoWorker().getAllWorkers();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        globalWorkersList.addAll(workersList);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
             }
         }).start();
-
     }
 }
